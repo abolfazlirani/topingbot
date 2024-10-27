fd89cde3875bc78a18a5a386355504557de5eaaa
abolfazl <abiha9480@gmail.com>
2024-10-27 10:42:59 +0330

    first

--- bin/topingbot.dart
+++ bin/topingbot.dart
@@ -1,5 +1,22 @@
-import 'package:topingbot/topingbot.dart' as topingbot;
+import 'package:televerse/televerse.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${topingbot.calculate()}!');
+void main() {
+  // توکن ربات تلگرام خود را وارد کنید
+  final bot = Bot('7830294069:AAHWZLtw7-xfewH0JJ_l8W-oWUa0Gg6BKEM');
+
+  // تعریف دستور /start
+  bot.command('start', (ctx) {
+    ctx.reply('سلام! به ربات تلگرام خوش آمدید.');
+  });
+
+  // تعریف دستور /echo
+  bot.command('echo', (ctx) {
+    if (ctx.message!.text != null) {
+      String userMessage = ctx.message!.text!.replaceFirst('/echo', '').trim();
+      ctx.reply(userMessage.isNotEmpty ? userMessage : 'متنی وارد نشده است!');
+    }
+  });
+
+  // راه‌اندازی ربات
+  bot.start();
 }
